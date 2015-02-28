eaf74ce33b2a1ea0ef3305f9fc6d360679b29896
jmiooo <leet2013@gmail.com>
2015-02-28 14:23:39 -0500

    Starting code!

---
+++ Elemental Fighting Platformer/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+	public float distance;
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		distance = 30.0f;
+		player = GameObject.Find ("Player");
+		transform.position = player.transform.position + new Vector3(0, distance * Mathf.Sin(Mathf.PI / 4), -distance * Mathf.Cos(Mathf.PI / 4));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 desiredPosition = player.transform.position + new Vector3 (0, distance * Mathf.Sin (Mathf.PI / 4), -distance * Mathf.Cos (Mathf.PI / 4));
+		if (transform.position != desiredPosition)
+		{
+			rigidbody.velocity = 5.0f * (desiredPosition - transform.position);
+		}
+		else
+		{
+			rigidbody.velocity.Set(0.0f, 0.0f, 0.0f);
+		}
+	}
+}
---
+++ Elemental Fighting Platformer/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+	public float WALK_FORCE = 10.0f;
+	public float JUMP_FORCE = 200.0f;
+	public float MAX_SPEED = 50.0f;
+
+	private bool grounded;
+	private int direction;
+
+	private GameObject playerSprite;
+	private GameObject groundCheck;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		playerSprite = transform.FindChild("PlayerSprite").gameObject;
+		//groundCheck = transform.FindChild("GroundCheck").gameObject;
+		anim = playerSprite.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	/*void Update () {
+		float h, v;
+
+		h = Input.GetAxis("Horizontal");
+		v = Input.GetAxis("Vertical");
+
+		if (Mathf.Abs(h) > 0.0f || Mathf.Abs(v) > 0.0f) {
+			anim.SetInteger("Move", 1);
+			if (Mathf.Abs(h) > 0.0f)
+				anim.SetInteger("Direction", h > 0.0f ? 1 : -1);
+		}
+		else {
+			anim.SetInteger("Move", 0);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rigidbody.AddForce(new Vector3(0, Mathf.Sin(2 * Mathf.PI / 3) * 100.0f, Mathf.Cos(2 * Mathf.PI / 3) * 100.0f));
+		}
+	}
+
+	void FixedUpdate() {
+		float h, v;
+
+		Vector3 moveForce;
+
+		h = Input.GetAxis("Horizontal");
+		v = Input.GetAxis("Vertical");
+		moveForce = new Vector3(10.0f * h, Mathf.Sin(Mathf.PI / 6) * 10.0f * v, Mathf.Cos(Mathf.PI / 6) * 10.0f * v);
+
+		rigidbody.AddForce(moveForce);
+	}*/
+	void Update () {
+		//grounded = Physics.Spherecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")); 
+
+		float h, v;
+
+		h = Input.GetAxis("Horizontal");
+		v = Input.GetAxis("Vertical");
+
+		if (Mathf.Abs(h) > 0.0f || Mathf.Abs(v) > 0.0f) {
+			anim.SetInteger("Move", 1);
+			if (Mathf.Abs(h) > 0.0f)
+				anim.SetInteger("Direction", h > 0.0f ? 1 : -1);
+		}
+		else {
+			anim.SetInteger("Move", 0);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rigidbody.AddForce(new Vector3(0, 200.0f, 0));
+		}
+	}
+
+	void FixedUpdate() {
+		float h, v;
+
+		Vector3 moveForce;
+
+		h = Input.GetAxis("Horizontal");
+		v = Input.GetAxis("Vertical");
+		moveForce = new Vector3(10.0f * h, 0, 10.0f * v);
+
+		rigidbody.AddForce(moveForce);
+	}
+}
