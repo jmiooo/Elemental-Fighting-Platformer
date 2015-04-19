f6f432b24b8d4c24d131fa354e12a8a6a9c17960
BarryLiCMU <barryl@andrew.cmu.edu>
2015-04-19 13:02:37 -0400

    Projectile Bugfix
    Fixed bug where player's projectiles were not colliding with props

--- Elemental Fighting Platformer/Assets/Scripts/ProjectileScript.cs
+++ Elemental Fighting Platformer/Assets/Scripts/ProjectileScript.cs
@@ -17,14 +17,14 @@ public class ProjectileScript : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D (Collider2D col) {
-		if(parentTag == "Enemy" && (col.tag == "Player" || col.tag == "Prop"))
-		{
+		if (parentTag == "Enemy" && col.tag == "Player") {
 			Destroy (gameObject);
-		}
-		else if (parentTag == "Player" && col.tag == "Enemy") {
+		} else if (parentTag == "Player" && col.tag == "Enemy") {
 			//col.gameObject.GetComponent<EnemyScript>().takeDamage(10);
-			col.gameObject.GetComponent<EnemyScript>().takeElementAndDamage(element, 10);
-			Destroy(gameObject);
+			col.gameObject.GetComponent<EnemyScript> ().takeElementAndDamage (element, 10);
+			Destroy (gameObject);
+		} else if (col.tag == "Prop") {
+			Destroy (gameObject);
 		}
 	}
 }
