138cfaa0a190b8f22d5eaec949d06f62c0489db4
BarryLiCMU <barryl@andrew.cmu.edu>
2015-04-19 13:59:51 -0400

    Enemy Spawner
    Spawner spawns GameObjects at a Vector2 position after a set amount of
    time has passed

---
+++ Elemental Fighting Platformer/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class EnemyInit {
+	public GameObject enemy;
+	public Vector2 position;
+	public float timer;
+
+	private float lastTime;
+	private bool spawned = false;
+
+	public void setTime(float time) {
+		lastTime = time;
+	}
+
+	public float getTime() {
+		return lastTime;
+	}
+
+	public void setSpawned (bool spawn) {
+		spawned = spawn;
+	}
+
+	public bool getSpawned () {
+		return spawned;
+	}
+
+	public bool checkTime () {
+		if (Time.fixedTime - lastTime > timer) {
+			lastTime = Time.fixedTime;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
+
+public class EnemySpawner : MonoBehaviour {
+
+	//public GameObject[] enemies = new GameObject[enemyCount];
+	public EnemyInit[] enemies;
+
+	void Start() {
+		for (int i = 0; i < enemies.Length; i++) {
+			enemies [i].setTime (Time.fixedTime);
+			enemies [i].setSpawned (false);
+		}
+	}
+
+	void Update () {
+		for (int i = 0; i < enemies.Length; i++) {
+			if (enemies[i].enemy != null && !enemies[i].getSpawned() &&
+			    Time.fixedTime - enemies[i].getTime() > enemies[i].timer) {
+				Instantiate(enemies[i].enemy, enemies[i].position, 
+				            Quaternion.Euler(new Vector3(0,0,0)));
+				enemies[i].setSpawned(true);
+			}
+		}
+	}
+}
