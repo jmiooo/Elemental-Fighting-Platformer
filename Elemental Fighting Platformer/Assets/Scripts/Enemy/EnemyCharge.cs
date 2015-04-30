df388a9f5d78503e4da9ae71323431524d700fc3
BarryLiCMU <barryl@andrew.cmu.edu>
2015-04-29 23:04:38 -0400

    Charging Enemy
    Added Charging and EnemyCollision

---
+++ Elemental Fighting Platformer/Assets/Scripts/Enemy/EnemyCharge.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCharge : MonoBehaviour {
+
+	public float radius;
+	public float speed;
+	public float timer;
+	public float chargespeed;
+	public float chargeTime;
+	
+	private Vector2 direction;
+	private Vector2 startpos;
+	private Vector2 target;
+	private Vector2 accel;
+	private float lastTime;
+	private bool charge;
+	
+	// Use this for initialization
+	void Start () {
+		startpos = gameObject.transform.position;
+		lastTime = Time.fixedTime;
+		charge = false;
+		
+		target = (Vector2)((Random.insideUnitCircle.normalized * radius)) + startpos;
+		direction = target - (Vector2)gameObject.transform.position;
+		accel = (direction - gameObject.rigidbody2D.velocity).normalized;
+		gameObject.rigidbody2D.velocity += (accel * speed);
+	}
+	
+	void FixedUpdate () {
+		if (!charge) {
+			if (Time.fixedTime - lastTime > timer) {
+				target = (Vector2)((Random.insideUnitCircle.normalized * radius)) + startpos;
+				lastTime = Time.fixedTime;
+			}
+			direction = target - (Vector2)gameObject.transform.position;
+			accel = (direction - gameObject.rigidbody2D.velocity).normalized;
+			gameObject.rigidbody2D.velocity += (accel * speed);
+			RaycastHit2D[] downhit = Physics2D.RaycastAll(transform.position, -Vector2.up);
+			if (downhit != null) {
+				foreach (RaycastHit2D hit in downhit) {
+					if(hit.collider.tag == "Player") {
+						Debug.Log("HIT");
+						charge = true;
+						direction = -Vector2.up;
+						lastTime = Time.fixedTime;
+					}
+				}
+			}
+			RaycastHit2D[] uphit = Physics2D.RaycastAll(transform.position, Vector2.up);
+			if (uphit != null) {
+				foreach (RaycastHit2D hit in uphit) {
+					if(hit.collider.tag == "Player") {
+						Debug.Log("HIT");
+						charge = true;
+						direction = Vector2.up;
+						lastTime = Time.fixedTime;
+					}
+				}
+			}
+			RaycastHit2D[] righthit = Physics2D.RaycastAll(transform.position, Vector2.right);
+			if (righthit != null) {
+				foreach (RaycastHit2D hit in righthit) {
+					if(hit.collider.tag == "Player") {
+						Debug.Log("HIT");
+						charge = true;
+						direction = Vector2.right;
+						lastTime = Time.fixedTime;
+					}
+				}
+			}
+			RaycastHit2D[] lefthit = Physics2D.RaycastAll(transform.position, -Vector2.right);
+			if (lefthit != null) {
+				foreach (RaycastHit2D hit in lefthit) {
+					if(hit.collider.tag == "Player") {
+						Debug.Log("HIT");
+						charge = true;
+						direction = -Vector2.right;
+						lastTime = Time.fixedTime;
+					}
+				}
+			}
+		} else {
+			gameObject.rigidbody2D.velocity = direction * chargespeed;
+			if (Time.fixedTime - lastTime > chargeTime) {
+				charge = false;
+			}
+		}
+
+	}
+}
---
+++ Elemental Fighting Platformer/Assets/Scripts/Enemy/EnemyCollision.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCollision : MonoBehaviour {
+	
+	public Constants.Elements element;
+	public int damage;
+	public float timer;
+
+	private bool collided;
+	private MovementScript2D playerScript;
+	private float lastTime;
+
+	void Start() {
+		collided = false;
+		lastTime = Time.fixedTime;
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.tag == "Player") {
+			collided = true;
+			lastTime -= timer;
+			playerScript = col.gameObject.GetComponent<MovementScript2D>();
+		}
+	}
+	void OnCollisionExit2D (Collision2D col) {
+		if(col.gameObject.tag == "Player") {
+			collided = false;
+		}
+	}
+
+	void FixedUpdate() {
+		if (collided && Time.fixedTime - lastTime > timer) {
+			playerScript.takeElementAndDamage(element, damage);
+			lastTime = Time.fixedTime;
+		}
+	}
+}
