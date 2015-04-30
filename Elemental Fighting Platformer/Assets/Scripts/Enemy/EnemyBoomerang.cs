6fb9b8573b78d46f8eebf9a751373dfaeecb9452
BarryLiCMU <barryl@andrew.cmu.edu>
2015-04-29 23:55:59 -0400

    Boomerang Enemy
    Enemy moves circularly towards player

---
+++ Elemental Fighting Platformer/Assets/Scripts/Enemy/EnemyBoomerang.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBoomerang : MonoBehaviour {
+
+	public Vector2 center;
+	private Vector2 pos = new Vector2();
+	public float aspeed, speed, radius, angle_init;
+	private float angle, ang_speed;
+	public bool is_clockwise;
+	
+	private GameObject player;
+	private Vector2 target;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		/* associate the enemy with the player */
+		player = GameObject.Find("Player");
+		//target = player.transform.position;
+		ang_speed = aspeed / radius;
+		angle = 0;
+		if (is_clockwise)
+			ang_speed *= -1;
+		Debug.Log ("Starting position: " + pos);
+		center = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		center = Vector2.MoveTowards (center, player.transform.position, speed);
+		float d_angle = ang_speed * Time.deltaTime;
+		pos.x = radius * Mathf.Cos (angle + d_angle) + center.x;
+		pos.y = radius * Mathf.Sin (angle + d_angle) + center.y;
+		transform.position = pos;
+		angle += d_angle;
+	}
+}
